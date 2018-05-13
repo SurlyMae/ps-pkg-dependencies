3e9fd37d595cc98116ec6514a03546552b6bdaef
SurlyMae <surlymae@gmail.com>
2018-05-12 21:07:58 -0600

    Name refactoring

--- tests/pkg-installer.test.js
+++ tests/pkg-installer.test.js
@@ -1,15 +1,15 @@
 const test = require('tape')
-const installOrder = require('../lib/pkg-installer.js')
+const getInstallOrder = require('../lib/pkg-installer.js')
 
-test('installOrder([ "KittenService: CamelCaser", "CamelCaser: " ]) should return "CamelCaser,KittenService" ', function(t) {
-  var result = installOrder(['KittenService: CamelCaser', 'CamelCaser: '])
+test('getInstallOrder([ "KittenService: CamelCaser", "CamelCaser: " ]) should return "CamelCaser,KittenService" ', function(t) {
+  var result = getInstallOrder(['KittenService: CamelCaser', 'CamelCaser: '])
   var expected = 'CamelCaser, KittenService'
   t.deepEqual(result, expected)
   t.end()
 })
 
-test('installOrder(["KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService","Fraudstream: Leetmeme","Ice: "]) should return "KittenService, CamelCaser, Ice, Cyberportal, Leetmeme, Fraudstream" ', function(t) {
-  var result = installOrder([
+test('getInstallOrder(["KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: "]) should return "KittenService, CamelCaser, Ice, Cyberportal, Leetmeme, Fraudstream" ', function(t) {
+  var result = getInstallOrder([
     'KittenService: ',
     'Leetmeme: Cyberportal',
     'Cyberportal: Ice',
@@ -19,6 +19,20 @@ test('installOrder(["KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ic
   ])
   var expected =
     'KittenService, CamelCaser, Ice, Cyberportal, Leetmeme, Fraudstream'
+  t.deepEqual(result, expected)
+  t.end()
+})
+
+test('getInstallOrder(["KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: ", "Ice: Leetmeme"]) should return "invalid input" ', function(t) {
+  var result = getInstallOrder([
+    'KittenService: ',
+    'Leetmeme: Cyberportal',
+    'Cyberportal: Ice',
+    'CamelCaser: KittenService',
+    'Fraudstream: Leetmeme',
+    'Ice: '
+  ])
+  var expected = 'invalid input'
   t.deepEqual(result, expected)
   t.end()
 })

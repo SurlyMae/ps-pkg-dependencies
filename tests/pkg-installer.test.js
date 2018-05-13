85cc45b1482c6630df193127abb07a5a2d2990e9
SurlyMae <surlymae@gmail.com>
2018-05-12 18:21:07 -0600

    Test 2

--- tests/pkg-installer.test.js
+++ tests/pkg-installer.test.js
@@ -1,2 +1,9 @@
 const test = require('tape')
 const installOrder = require('../lib/pkg-installer.js')
+
+test('installOrder([ "KittenService: CamelCaser", "CamelCaser: " ]) should return "CamelCaser,KittenService" ', function(t) {
+  var result = installOrder(['KittenService: CamelCaser', 'CamelCaser: '])
+  var expected = 'CamelCaser, KittenService'
+  t.deepEqual(result, expected)
+  t.end()
+})
